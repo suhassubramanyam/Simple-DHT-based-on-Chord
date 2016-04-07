0efb6e9de36b3145a3af07fab4db0b5bd869101b
suhas subramanyam <suhassub@buffalo.edu>
2016-04-06 22:50:51 -0400

    2nd Submission Code

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -308,28 +308,24 @@ public class SimpleDhtProvider extends ContentProvider {
         return 0;
     }
 
-    private JSONArray cur2Json(Cursor cursor) {
+    private JSONArray cur2Json(Cursor cursor) throws JSONException{
 
         JSONArray resultSet = new JSONArray();
         cursor.moveToFirst();
+
         while (!cursor.isAfterLast()) {
             int totalColumn = cursor.getColumnCount();
             JSONObject rowObject = new JSONObject();
             for (int i = 0; i < totalColumn; i++) {
                 if (cursor.getColumnName(i) != null) {
-                    try {
-                        rowObject.put(cursor.getColumnName(i),
-                                cursor.getString(i));
-                    } catch (Exception e) {
-                        Log.d(TAG, e.getMessage());
-                    }
+                    rowObject.put(cursor.getColumnName(i),
+                            cursor.getString(i));
                 }
             }
             resultSet.put(rowObject);
             cursor.moveToNext();
         }
 
-        //cursor.close();
         return resultSet;
 
     }
